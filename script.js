c54f51af07701af4ba9b2c69ada600b1ef7fae53
Glitch (glitch-hello-website) <none>
2021-07-22 00:05:46 +0000

    🚕🎩 Checkpoint ./script.js:16231546/732

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ https://public-apis.io/random-fox-animals-api
 https://icanhazdadjoke.com/api
 https://api.adviceslip.com/
 */
+
+const getJoke = async () => {
+  let joke;
+  fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+        .then(function (response) {
+            joke = response.json();
+        })
+        .then(function (data) {
+            console.log(joke);
+        })
+}
+
+getJoke();
